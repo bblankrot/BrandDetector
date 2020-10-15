26c06b5c0c9485e8486bcc2b7db628847bf1fe64
bblankrot <34473471+bblankrot@users.noreply.github.com>
2020-10-15 23:43:53 +0200

    added simple re test

--- tests/test_re.py
+++ tests/test_re.py
@@ -1,6 +1,14 @@
-import numpy.testing
-
+import spacy
 import brand_detector as bd
 
-if __name__ == "__main__":
-    1
+def test_blank_model_smoke():
+    nlp = spacy.blank("en")
+
+def test_find_brands():
+    text = "Hello T-Mobile for a limited time only. aDunkin! Dunkin!"
+    ans1 = bd.utils.find_brands("T mobile", text, ignore_case=True, dehyphenate=False)
+    assert ans1 == []
+    ans2 = bd.utils.find_brands("T mobile", text, ignore_case=True, dehyphenate=True)
+    assert ans2 == [(6, 14)]
+    ans3 = bd.utils.find_brands("Dunkin'", text, ignore_case=False, dehyphenate=True)
+    assert ans3 == [(31, 37)]
